549a8b7c35b0a2174e66dcba5c939681d7a1f067
Jon Maddox <jm@madev.me>
2020-08-16 17:25:33 -0500

    new ui

---
+++ frontend/src/App.svelte
@@ -0,0 +1,27 @@
+<script>
+    import Navbar from "./Navbar.svelte";
+    import Footer from "./Footer.svelte";
+    import Preloader from "./Preloader.svelte";
+    import Sidebar from "./Sidebar.svelte";
+    import Content from "./Content.svelte";
+
+    export let name;
+</script>
+
+<Navbar/>
+
+    <Preloader/>
+
+    <div class="container-fluid" style="margin-top: 6em">
+        <div class="row">
+            <Sidebar/>
+            <main class="col-md-9 col-lg-10">
+                <Content/>
+            </main>
+        </div>
+    </div>
+
+
+
+
+<Footer/>
---
+++ frontend/src/Content.svelte
@@ -0,0 +1,61 @@
+<script>
+    import { fade } from 'svelte/transition';
+
+    let editing = false;
+</script>
+
+<style>
+    #input-test:hover {
+        background-color: #f0f0f0;
+        cursor: pointer;
+    }
+
+    .hovered {
+        background-color: #f0f0f0;
+        cursor: pointer;
+    }
+</style>
+
+{#if editing}
+    <div class="container">
+        <div class="row">
+            <div class="col">
+                <div transition:fade={{duration : 300}} class="mb-3" on:click={() => editing = true}>
+                    <h3>Field Configuration</h3>
+                    <div style="padding: .85em 1em; border-radius: 1em">
+                        <label for="formGroupExampleInput2" class="form-label">Name</label>
+                        <input type="text" class="form-control" id="formGroupExampleInput2" placeholder="Another input placeholder">
+                    </div>
+                </div>
+            </div>
+            <div class="col">
+                <div class="mb-3 hovered" on:click={() => editing = true}>
+                    <div style="padding: .85em 1em; border-radius: 1em">
+                        <label for="formGroupExampleInput2" class="form-label">Another label</label>
+                        <input type="text" class="form-control" id="formGroupExampleInput2" placeholder="Another input placeholder">
+                    </div>
+                </div>
+                <div class="mb-3" on:click={() => editing = true}>
+                    <div style="padding: .85em 1em; border-radius: 1em">
+                        <label for="formGroupExampleInput2" class="form-label">Another input</label>
+                        <input type="text" class="form-control" id="formGroupExampleInput2" placeholder="Another input placeholder">
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+{:else}
+    <div class="mb-3" id="input-test" on:click={() => editing = true}>
+        <div style="padding: .85em 1em; border-radius: 1em">
+            <label for="formGroupExampleInput2" class="form-label">Another label</label>
+            <input type="text" class="form-control" id="formGroupExampleInput2" placeholder="Another input placeholder">
+        </div>
+    </div>
+    <div class="mb-3" id="input-test-2" on:click={() => editing = true}>
+        <div style="padding: .85em 1em; border-radius: 1em">
+            <label for="formGroupExampleInput2" class="form-label">Another label</label>
+            <input type="text" class="form-control" id="formGroupExampleInput2" placeholder="Another input placeholder">
+        </div>
+    </div>
+{/if}
+
