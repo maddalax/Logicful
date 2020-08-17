4596d70fd9f9247ea79d5102ef6600a261d30d0d
Maddox <jm@madev.me>
2020-08-17 07:15:06 -0500

    files

---
+++ frontend/src/main.ts
@@ -0,0 +1,14 @@
+import App from './App.svelte';
+import 'dragula/dist/dragula.min.css'
+import {dispatch} from "event/EventBus";
+
+const app = new App({
+	target: document.body
+});
+
+
+document.addEventListener("click", (e) => {
+	dispatch("document_click", e);
+})
+
+export default app;
