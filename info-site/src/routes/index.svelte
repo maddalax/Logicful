639d1d0bda3943ff93fabd4b8526f5f345ec082e
Sydne Anschutz <sydne.anschutz@cerner.com>
2020-08-23 11:13:11 -0500

    landing page

--- info-site/src/routes/index.svelte
+++ info-site/src/routes/index.svelte
@@ -41,29 +41,126 @@
             <h3 class="display-4" style="padding-bottom: 2em;">A versatile online form builder for everyone
             </h3>
             <div class="col-md-6 col-xl-6 mb-5">
-                <img class="organic-radius img-fluid" src="../../assets/img/sections/about-us-1.jpg" alt="Office Desk">
+                <img class="organic-radius img-fluid" src="../../assets/img/sections/about-us-1.jpg" alt="Office Desk" style="height: 20em;">
             </div>
             <div class="col-md-6 col-xl-5 text-center text-md-left" style="padding-left: 2em;">
-                <h4 class="h4 mb-3">All challenges accepted.</h4>
-                <p class="">Themesberg is an experienced and passionate group of designers, developers, project
-                    managers, writers and artists. Every client we work with becomes a part of the team. Together we
-                    face the challenges and celebrate the victories.</p>
+                <h4 class="h4 mb-3">Build something amazing.
+                </h4>
+                <p class="">Create beautiful online forms without knowing how to code. With an intuitive drag-and-drop form builder and hundreds of templates at your fingertips, the possibilities are endless.
+                </p>
             </div>
 
             <div class="col-md-6 col-xl-5 text-center text-md-left">
-                <h4 class="h4 mb-3">All challenges accepted.</h4>
-                <p class="">With a culture of collaboration, a roster of talent, and several office pooches, the
-                    Themesberg team is active in the creative community, endlessly interested in what’s next, and
-                    generally pleasant to be around.</p>
+                <h4 class="h4 mb-3">Get more from your data.
+                </h4>
+                <p class="">Give your workflows a powerful kick start. Send information to the people and places that need it by connecting your online forms to dozens of native apps or custom webhooks.
+                </p>
             </div>
             <div class="col-md-6 col-xl-6 mb-5">
-                <img class="organic-radius img-fluid" src="../../assets/img/sections/about-us-1.jpg" alt="Office Desk">
+                <img class="organic-radius img-fluid" src="../../assets/img/sections/about-us-1.jpg" alt="Office Desk" style="height: 20em;">
             </div>
         </div>
     </div>
 </section>
 
 
+<!--Use Cases-->
+<section class="section section-md" style="padding-top: 9em; padding-bottom: 2em;">
+    <div class="container">
+        <div class="col-12 col-lg-12">
+            <div class="row justify-content-center text-center mt-n7 z-2">
+                <h3 class="h3" style="padding-bottom: 1.5em;">Powerful solutions for teams of all shapes and sizes
+                </h3>
+
+                <div class="col-12 col-md-6">
+                    <div class="row mt-5 mt-lg-0 px-3">
+                        <!-- Registrations Box -->
+                        <div class="card border-light">
+                            <div class="card-body pt-3 pb-3 pt-lg-4 pb-lg-4" style="padding-right: 0em; padding-left: 0em;">
+                                <div class="row mt-5 mt-lg-0 px-3">
+                                    <div class="col-3 col-md-3 pr-0 pl-0 pr-0">
+                                        <i class="far fa-address-card" style="font-size: 3rem;"/>
+                                    </div>
+                                    <div class="col-9 col-md-9 text-left pl-0 pr-0">
+                                        <h5 class="h5 mb-2">Registrations</h5>
+                                        <p>
+                                            Streamline your online event management process.
+                                            Use event management automation to seamlessly organize your events,
+                                            boost online event registrations, manage tickets, and more.                                        </p>
+<!--                                        <a href="#" class="btn btn-sm btn-primary">Learn more</a>-->
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+                        <!-- Order Forms Box -->
+                        <div class="card border-light mt-3">
+                            <div class="card-body pt-3 pb-3 pt-lg-4 pb-lg-4" style="padding-right: 0em; padding-left: 0em;">
+                                <div class="row mt-5 mt-lg-0 px-3">
+                                    <div class="col-3 col-md-3 pr-0 pl-0 pr-0">
+                                        <i class="far fa-clipboard" style="font-size: 3rem;"/>
+                                    </div>
+                                    <div class="col-9 col-md-9 text-left pl-0 pr-0">
+                                        <h5 class="h5 mb-2">Order Forms</h5>
+                                        <p>
+                                            Start taking orders online without wasting additional time taking orders over the phone. Store all information directly in your Formstack database, accept online payments, and receive email notifications each time your cake order form is submitted.</p>
+<!--                                        <a href="#" class="btn btn-sm btn-primary">Learn more</a>-->
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+
+                <div class="col-12 col-md-6  mt-lg-5">
+                    <div class="row mt-5 mt-lg-0 px-3">
+                        <!-- Online Payment Box -->
+                        <div class="card border-light">
+                            <div class="card-body pt-3 pb-3 pt-lg-4 pb-lg-4" style="padding-right: 0em; padding-left: 0em;">
+                                <div class="row mt-5 mt-lg-0 px-3">
+                                                <div class="col-3 col-md-3 pr-0 pl-0 pr-0">
+                                                    <i class="far fa-credit-card" style="font-size: 3rem;"/>
+                                                </div>
+                                                <div class="col-9 col-md-9 text-left pl-0 pr-0">
+                                                    <h5 class="h5 mb-2">Online Payments</h5>
+                                                    <p>
+                                                        Use beautifully branded forms to collect payments online, automate your billing documentation, and boost sales. No messy coding needed.
+                                                    </p>
+<!--                                                    <a href="#" class="btn btn-sm btn-primary">Learn more</a>-->
+                                                </div>
+                                </div>
+                            </div>
+                        </div>
+
+                        <!-- Online Survey Box -->
+                        <div class="card border-light mt-3">
+                            <div class="card-body pt-3 pb-3 pt-lg-4 pb-lg-4" style="padding-right: 0em; padding-left: 0em;">
+                                <div class="row mt-5 mt-lg-0 px-3">
+                                    <div class="col-3 col-md-3 pr-0 pl-0 pr-0">
+                                        <i class="fas fa-envelope-open-text" style="font-size: 3rem;"/>
+                                    </div>
+                                    <div class="col-9 col-md-9 text-left pl-0 pr-0">
+                                        <h5 class="h5 mb-2">Online Surveys</h5>
+                                        <p>
+                                            Creating online surveys doesn't have to be difficult. Let Formstack’s online survey tool do the hard work for you so you can focus on getting the answers you need.                                </p>
+<!--                                        <a href="#" class="btn btn-sm btn-primary">Learn more</a>-->
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <!-- End of Icon Boxes -->
+                    </div>
+                </div>
+
+
+            </div>
+        </div>
+    </div>
+</section>
+
+
+<!--Subscribe-->
 <section class="section section-lg pt-0" style="padding-bottom: 2em;">
     <div class="container">
         <div class="row justify-content-center mb-2">
@@ -71,7 +168,9 @@
                 <div class="card border-0  p-md-3">
                     <div class="card-header bg-white border-0 text-center">
                         <h2>Subscribe</h2>
-                        <p>Join our mailing list.</p>
+                        <p>We're coming soon.
+                            Our website is under construction.
+                            Subscribe to be one of the first to use our product.</p>
                     </div>
                     <form action="#">
                         <div class="form-row mb-2">
@@ -100,7 +199,7 @@
     </div>
 </section>
 
-
+<!-- Final Word -->
 <div class="section bg-primary-app">
     <figure class="position-absolute top-0 left-0 w-100 d-none d-md-block mt-n3">
         <svg class="fill-primary-app" version="1.1" xmlns="http://www.w3.org/2000/svg"
@@ -112,26 +211,12 @@
     <div class="container">
         <div class="row row-grid align-items-center">
             <div class="col-12 col-lg-6">
-                <span class="h5 text-muted mb-2 d-block">Download App</span>
-                <h2 class="display-3 mb-4">Get started in seconds</h2>
-                <p class="lead text-muted">Quickly connect to tools and services such as Google Analytics, Google Search
-                    Console, Intercom or Github to track, measure and optimize performance. </p>
-                <div class="d-flex flex-wrap mt-4 mt-lg-5">
-                    <a href="#" class="btn btn-dark mb-xl-0 mr-3">
-                                <span class="d-flex align-items-center">
-                                    <span class="icon icon-brand mr-2"><span class="fab fa-apple"></span></span>
-                                    <span class="d-inline-block text-left">
-                                        <small class="font-weight-normal d-block">Available on</small> App Store
-                                    </span>
-                                </span>
-                    </a>
-                    <a href="#" class="btn btn-dark">
-                        <span class="icon icon-brand mr-2"><span class="fab fa-google-play"></span></span>
-                        <span class="d-inline-block text-left">
-                                    <small class="font-weight-normal d-block">Available on</small> Google Play
-                                </span>
-                    </a>
-                </div>
+                <h2 class="display-3 mb-4">No form transaction fees.</h2>
+                <p class="lead text-muted">Many ticketing apps charge their own transaction fees when you sell tickets. Sometimes, this doesn't even include the fees your payment gateway charges on their end! All of these added fees can result in less profit for your event.
+
+                    To help you save money, Formstack does not charge a transaction fee for tickets. However, you will still need to pay processing fees associated with your payment gateway when collecting online event registrations.
+                </p>
+
             </div>
             <div class="col-12 col-lg-5 ml-lg-auto">
                 <img class="d-none d-lg-inline-block" src="../../assets/img/illustrations/app-landing-cta.svg"
