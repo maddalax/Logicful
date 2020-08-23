c8f29560ceadf6fe4de638a9b0977bf4571f816b
Jon Maddox <jm@madev.me>
2020-08-23 18:16:39 -0500

    additional logic checks

--- frontend/src/services/LogicBuilder.ts
+++ frontend/src/services/LogicBuilder.ts
@@ -8,7 +8,7 @@ export class LogicBuilder {
         if(!field.logic.rules || field.logic.rules.length === 0) {
             return true;
         }
-        console.log("FIELD LOGIC", field.logic);
+
         if (field.logic.action === "show-any-match") {
             for (let rule of field.logic.rules) {
                 const fieldTargetValue = formStore.get(rule.field);
@@ -21,6 +21,33 @@ export class LogicBuilder {
             }
             return false;
         }
+
+        if (field.logic.action === "show-all-match") {
+            for (let rule of field.logic.rules) {
+                const fieldTargetValue = formStore.get(rule.field);
+                if(fieldTargetValue == null) {
+                    return false;
+                }
+                if(!this.evaluateCondition(rule, fieldTargetValue)) {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        if (field.logic.action === "hide-all-match") {
+            for (let rule of field.logic.rules) {
+                const fieldTargetValue = formStore.get(rule.field);
+                if(fieldTargetValue == null) {
+                    return true;
+                }
+                if(!this.evaluateCondition(rule, fieldTargetValue)) {
+                    return true;
+                }
+            }
+            return false;
+        }
+
         if (field.logic.action === "hide-any-match") {
             for (let rule of field.logic.rules) {
                 const fieldTargetValue = formStore.get(rule.field);
